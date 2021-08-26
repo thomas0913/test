736c3a45d9d809abdbc9745db54ed281818a5898
thomas0913 <408440021@gms.tku.edu.tw>
2021-08-26 20:14:05 +0800

    for lesson one add workspace add package add hello_world.cpp

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/thomas/test/src
+# Build directory: /home/thomas/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("example")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/thomas/test/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/thomas/test/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/thomas/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/thomas/test/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/thomas/test/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/thomas/test/src:$ROS_PACKAGE_PATH"
