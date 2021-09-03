961251724ca527a80d8d970132513e71b119200e
thomas0913 <408440021@gms.tku.edu.tw>
2021-09-03 23:25:26 +0800

    for lesson three add C_two.cpp and C_three

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/thomas/test/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/thomas/test/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/thomas/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/thomas/test/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/thomas/test/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/thomas/test/src:$ROS_PACKAGE_PATH"
